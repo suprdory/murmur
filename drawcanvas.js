d89aa17757721db7f7693f5a7aca5b1f60f46bb9
nathan <suprdory@gmail.com>
2021-12-26 00:31:46 +0000

    we got borders

--- drawcanvas.js
+++ drawcanvas.js
@@ -1,49 +1,67 @@
-// function draw() {
-//         var canvas = document.getElementById('tutorial');
-// if (canvas.getContext) {
-//             var ctx = canvas.getContext('2d');
-//         }
-//     }
-
 const canvas = document.getElementById("cw");
 const context = canvas.getContext("2d");
-context.globalAlpha = 0.5;
+let particlesArray = [];
 
 const cursor = {
     x: innerWidth / 2,
     y: innerHeight / 2,
 };
 
+const dt = 0.1
+const dx = 1
+const du = 1
+const edgeWidth = 0.1
 class Particle {
-    constructor(x, y, particleTrailWidth, strokeColor, rotateSpeed) {
-        this.x = x;
-        this.y = y;
-        this.particleTrailWidth = particleTrailWidth;
+    constructor(x, y, u, v, strokeColor, rotateSpeed) {
+        this.x = x + randn_bm() * 50;
+        this.y = y + randn_bm() * 50;
+        this.u = u + randn_bm() * 10;
+        this.v = v + randn_bm() * 10;
+        this.size = 20;
+        this.alpha = 10 * Math.PI / 180;
         this.strokeColor = strokeColor;
-        this.theta = Math.random() * Math.PI * 2;
+        this.fillColor = strokeColor;
+        this.theta = Math.atan2(this.v,this.u);
         this.rotateSpeed = rotateSpeed;
         this.t = Math.random() * 150;
+    }
+    move()  {
+        this.x = this.x + this.u*  dt;
+        this.y = this.y + this.v * dt;
+        this.theta = Math.atan2(this.u,this.v)
+    }
 
-        this.rotate = () => {
-            const ls = {
-                x: this.x,
-                y: this.y,
-            };
-            this.theta += this.rotateSpeed;
-            this.x = cursor.x + Math.cos(this.theta) * this.t;
-            this.y = cursor.y + Math.sin(this.theta) * this.t;
-            context.beginPath();
-            context.lineWidth = this.particleTrailWidth;
-            context.strokeStyle = this.strokeColor;
-            context.moveTo(ls.x, ls.y);
-            context.lineTo(this.x, this.y);
-            context.stroke();
-        };
+    draw() {
+        let L=this.size
+        let a=this.alpha
+        let th=this.theta
+        context.beginPath();
+        context.fillStyle = this.fillColor;
+        context.moveTo(this.x, this.y);
+        context.lineTo(this.x - L*Math.sin(th-a), this.y - L*Math.cos(th-a));
+        context.lineTo(this.x - L*Math.sin(th+a), this.y - L*Math.cos(th+a));
+        context.fill();
+    }
+
+    detectEdge() {
+        if ( this.x > innerWidth * (1 - edgeWidth)){
+            this.u = this.u-du
+        }
+        if ( this.x < innerWidth * edgeWidth){
+            this.u = this.u + du
+        }
+        if ( this.y > innerHeight * (1 - edgeWidth)){
+            this.v = this.v - du
+        }
+        if ( this.y < innerHeight * edgeWidth){
+            this.v = this.v + du
+        }
+
     }
 }
-let particlesArray = [];
 
-generateParticles(101);
+
+generateParticles(100);
 setSize();
 anim();
 
@@ -69,7 +87,8 @@ function generateParticles(amount) {
         particlesArray[i] = new Particle(
             innerWidth / 2,
             innerHeight / 2,
-            4,
+            5,
+            5,
             generateColor(),
             0.02
         );
@@ -91,11 +110,21 @@ function setSize() {
 }
 
 
+// Standard Normal variate using Box-Muller transform.
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
 function anim() {
     requestAnimationFrame(anim);
 
-    context.fillStyle = "rgba(0,0,0,0.05)";
+    context.fillStyle = "rgba(0,0,0,1.0)";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    particlesArray.forEach((particle) => particle.rotate());
+    particlesArray.forEach((particle) => particle.move());
+    particlesArray.forEach((particle) => particle.draw());
+    particlesArray.forEach((particle) => particle.detectEdge());
 }
