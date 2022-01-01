527e7982008570c76aff1b025204bec82a04ae74
Nathan <suprdory@google.com>
2022-01-01 18:36:14 +0000

    tweak hsv randomizer

--- drawcanvas.js
+++ drawcanvas.js
@@ -18,6 +18,20 @@ const du = 0.1
 const dthmax=0.1
 const edgeWidth = 0.3
 
+function toggleBG() {
+    if (lightBG) {
+        lightBG = false
+        // document.body.style.backgroundColor = "black";
+        bgFillStyle = "rgba(0,0,0,0.050)"
+    }
+    else {
+        lightBG = true
+        // document.body.style.backgroundColor = "white";
+        bgFillStyle = "rgba(255,255,255,0.050)"
+    }
+
+}
+
 
 class Particle {
     constructor() {
@@ -31,7 +45,6 @@ class Particle {
         this.alpha = 10 * Math.PI / 180;
         this.fillColor = 0;
         this.theta = Math.atan2(this.v,this.u);
-
     }
 
     move()  {
@@ -193,8 +206,6 @@ addEventListener(
     { passive: false }
 );
 
-
-
 addEventListener("resize", () => setSize());
 
 function generateParticles(amount) {
@@ -202,37 +213,25 @@ function generateParticles(amount) {
         particlesArray[i] = new Particle();
     }
 }
-function generateHSLColor(hueWidth, hueStart, value) {
+function generateHSLColor(hueWidth, hueStart, valueWidth, valueStart) {
     // return 'hsl(' + Math.random()*360 + ' , 100%, 50%)';
-    let colorString = 'hsl(' + (Math.random() * hueWidth + hueStart) + ' , 100%, ' + value + '%)'
+    let colorString = 'hsl(' + (Math.random() * hueWidth + hueStart) + ' , 100%, ' + (Math.random() ** 2 * valueWidth + valueStart) + '%)'
     // console.log(colorString)
     return colorString;
 }
 
 function setColors() {
-    let hueWidth = Math.random() * 360
+    let hueWidth = Math.random() ** 2 * 360
     let hueStart = Math.random() * 360
-    let value = Math.random()**2 * 50 + 50
-    particlesArray.forEach((particle) => particle.setColor(generateHSLColor(hueWidth, hueStart, value)))
+    let valueStart = 50 + Math.random() ** 2 * 50
+    let valueWidth = Math.random() ** 2 * 50
+    particlesArray.forEach((particle) => particle.setColor(
+        generateHSLColor(hueWidth, hueStart, valueWidth, valueStart)))
 }
 
 function setSize() {
     canvas.height = innerHeight;
     canvas.width = innerWidth;
-}
-
-function toggleBG() {
-    if (lightBG) {
-        lightBG=false
-        // document.body.style.backgroundColor = "black";
-        bgFillStyle="rgba(0,0,0,0.05)"
-    }
-    else {
-        lightBG = true
-        // document.body.style.backgroundColor = "white";
-        bgFillStyle = "rgba(255,255,255,0.05)"
-    }
-
 }
 
 
